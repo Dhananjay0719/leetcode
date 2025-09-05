b022989d902223c051018c391e589b41ff5f038a
Dhananjay Badoni <101263267+Dhananjay0719@users.noreply.github.com>
2025-09-05 12:08:27 +0530

    Time: 0 ms (100%), Space: 63.1 MB (12.32%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int maxArea = 0;
 
-        while (left < right) {
-            int currentArea = min(height[left], height[right]) * (right - left);
-            maxArea = max(maxArea, currentArea);
+        //Bruteforce Solution
+        // int maxwater=0;
+        // for(int i=0;i<height.size()-1;i++){
+        //     for(int j=i+1;j<height.size();j++){
+        //         int sum= (j-i) * min(height[i],height[j]);   //l*b
+        //         if(maxwater<sum){
+        //             maxwater=sum;
+        //         }
+        //     }
+        // }
 
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+        // return maxwater;
+
+        int maxwater=0;
+        int first=0,last=height.size()-1;
+        
+        while(first<last){
+         int length=last-first;
+         int breadth=min(height[first],height[last]);
+         int sum=length*breadth;
+         if(sum>maxwater){maxwater=sum;}
+
+         if(height[first]<height[last]){first++;}
+         else{last--;}
         }
-
-        return maxArea;
+        return maxwater;
     }
 };
