87431969ea7d6b3b10eaa76054e651f849f9c489
Dhananjay Badoni <101263267+Dhananjay0719@users.noreply.github.com>
2025-09-13 13:43:00 +0530

    Time: 3 ms (37.49%), Space: 9.6 MB (71.86%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.cpp
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        map<char,int> mv;
-        map<char,int> mc;
+        // map<char,int> mv;
+        // map<char,int> mc;
+        // int maxfreqv=0,maxfreqc=0;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+        //         mv[s[i]]++;
+        //     }
+        //     else{
+        //         mc[s[i]]++;
+        //     }
+        // }
+
+        // for(auto &p: mv){
+        //     if(p.second>maxfreqv){
+        //         maxfreqv=p.second;
+        //     }
+        // }
+
+        // for(auto &p: mc){
+        //     if(p.second>maxfreqc){
+        //         maxfreqc=p.second;
+        //     }
+        // }
+        
+        //One Hashmap
         int maxfreqv=0,maxfreqc=0;
+
+        map<char,int>mpp;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
-                mv[s[i]]++;
+           if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                mpp[s[i]]++;
+                maxfreqv=max(maxfreqv,mpp[s[i]]);
             }
             else{
-                mc[s[i]]++;
+                mpp[s[i]]++;
+                maxfreqc=max(maxfreqc,mpp[s[i]]);
             }
         }
-
-        for(auto &p: mv){
-            if(p.second>maxfreqv){
-                maxfreqv=p.second;
-            }
-        }
-
-        for(auto &p: mc){
-            if(p.second>maxfreqc){
-                maxfreqc=p.second;
-            }
-        }
-
         return maxfreqv+maxfreqc;
     }
 };
